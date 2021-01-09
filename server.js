43cd6eab787febc741615eb7eef9a8358e5115af
cratermaik <cratermaik@gmail.com>
2021-01-09 11:59:51 -0500

    update index to server and connection socket

---
+++ server.js
@@ -0,0 +1,11 @@
+let socket = require('socket.io')
+
+let io = socket.listen(8080)
+
+io.sockets.on('connection', function (socket) {
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data)
+
+  })
+  
+})
