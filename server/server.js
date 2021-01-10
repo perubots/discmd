d5b2c79d698d0c9e7e3989a9058a97a45d7592cd
cratermaik <cratermaik@gmail.com>
2021-01-09 21:41:35 -0500

    update dotenv and WH

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-require('dotenv').config
-let URL_WEBHOOK = `https://discord.com/api/v8/webhooks/${process.env.ID_WEBHOOK}/${process.env.TOKEN_WEBHOOK}`
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
@@ -7,24 +6,38 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const fetch = require('node-fetch')
 
+let URLWH = `https://discord.com/api/v8/webhooks/${process.env.ID_WEBHOOK}/${process.env.TOKEN_WEBHOOK}`
+
 io.on('connection', function (socket) {
   socket.on('send', function (data) {
+    console.log(data);
+    let username, content
+
+    if(data.type == 'log') {
+      username = 'System Discmd'
+      content = `**__${data.message}__**`
+    } else {
+      username = data.nick
+      content = data.message
+    }
+
     const body = JSON.stringify({
       allowed_mentions: {
         parse: []
       },
-      content: data.message,
-      username: data.nick,
+      content: content,
+      username: username,
       avatar_url: 'https://aux2.iconspalace.com/uploads/utilities-terminal-icon-256.png'
     })
-
-    fetch(URL_WEBHOOK, {
+  
+    fetch(URLWH, {
       method: 'POST',
       body: body,
       headers: {
         'Content-Type': 'application/json'
       }
     })
+
     socket.broadcast.emit('message', data)
 
   })
